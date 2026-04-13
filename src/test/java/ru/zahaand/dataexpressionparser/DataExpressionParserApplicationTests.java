1233cada2fa221e0d4884bec2a4b33d7587534e5
a.zakharenkov <a.zakharenkov@mkskom.ru>
2026-04-13 14:35:35 +0300

    initial commit

---
+++ src/test/java/ru/zahaand/dataexpressionparser/DataExpressionParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zahaand.dataexpressionparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExpressionParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
